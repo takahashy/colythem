ac7cb994e15be55b08d4c2fc46da35cd13eabfff
takahashy <takahashytufts@gmail.com>
2023-03-28 17:33:07 -0400

    added comments

--- COLYTHEM/Assets/Scripts/ColorSwitcher.cs
+++ COLYTHEM/Assets/Scripts/ColorSwitcher.cs
@@ -11,15 +11,18 @@ public class ColorSwitcher : MonoBehaviour
 
     void Update()
     {
+        // Press 'r' to switch to red
         if (Input.GetKeyDown(KeyCode.R)) {
             spriteRenderer.material.color = Color.red;
         }
 
+        // Press 'b' to switch to blue
         else if (Input.GetKeyDown(KeyCode.B))
         {
             spriteRenderer.material.color = Color.blue;
         }
 
+        // Press 'space' to switch to white
         else if (Input.GetKeyDown(KeyCode.Space))
         {
             spriteRenderer.material.color = Color.white;
