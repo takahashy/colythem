6191d0b0305b1a4637bd66bd9426e0f4aeef13f6
Overal404 <125411174+Overal404@users.noreply.github.com>
2023-03-29 17:27:18 -0400

    Pause menu in new scene BeatUI
    3 bugs:
    buttons aren't doing anything for some reason
    Volume mixer just wont work, getting a warning for some reason
    Text for get lives isn't working either

---
+++ COLYTHEM/Assets/Scripts/PauseHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PauseHandler : MonoBehaviour
+{
+    public GameObject playUI;
+    public GameObject pauseUI;
+    public static bool paused;
+    public AudioMixer mixer;
+    public static float volumeLevel = 1.0f;
+    private Slider sliderVolumeCtrl;
+
+    void Awake ()
+    {
+        SetLevel(volumeLevel);
+    }
+
+    void Start()
+    {
+        Resume();
+    }
+    
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("Volume", Mathf.Log10 (sliderValue) * 20);
+        volumeLevel = sliderValue;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        playUI.SetActive(true);
+        pauseUI.SetActive(false);
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        playUI.SetActive(false);
+        pauseUI.SetActive(true);
+        paused = true;
+    }
+
+    public void QuitGame(){
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
+
