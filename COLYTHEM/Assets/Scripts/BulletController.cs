168d7f9707d5d6c803cba7cbc75903f964071456
Silvia Wang <w.silvia343@gmail.com>
2023-04-24 21:53:23 -0400

    animation and random shooting

--- COLYTHEM/Assets/Scripts/BulletController.cs
+++ COLYTHEM/Assets/Scripts/BulletController.cs
@@ -6,16 +6,34 @@ public class BulletController : MonoBehaviour
 {
     public float xspeed = 0f;
     public float yspeed = 0f;
+    private int randomValue;
+    // public SpriteRenderer treble;
+    // public SpriteRenderer bass;
+    // public SpriteRenderer eighth;
+    // public SpriteRenderer eighthrest;
+    // public SpriteRenderer flat;
+    // public SpriteRenderer half;
+    // public SpriteRenderer halfdot;
+    // public SpriteRenderer halfrest;
+    // public SpriteRenderer qrest;
+    // public SpriteRenderer quarter;
+    // public SpriteRenderer treble;
+    // public SpriteRenderer whole;
+    // public SpriteRenderer wholerest;
+    public Sprite[] spriteList;
 
     // Start is called before the first frame update
     void Start()
     {
+         int randomValue = Random.Range(0, 14);
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteList[randomValue];
         StartCoroutine(DestroyBullet());
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         Vector2 position = transform.position;
         position.x += xspeed;
         position.y += yspeed;
