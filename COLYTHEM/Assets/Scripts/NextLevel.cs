0ab06438f5f4243c81a60afc83e48f0b202c77d6
Nicholas Chen <nicholas.k.y.chen@gmail.com>
2023-04-24 20:32:34 -0400

    Added scene switching

---
+++ COLYTHEM/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    // Reference to the player's GameObject
+    public GameObject player;
+
+    // Reference to the box collider on the object that triggers the scene change
+    public BoxCollider2D sceneTriggerCollider;
+
+    // The name of the scene to load
+    public string sceneToLoad;
+
+    // Detects collision with the player
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            // Load the scene when collision occurs
+            Debug.Log("testing");
+            // SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
